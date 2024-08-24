60c5a973c37c5023d1ca54adaaf6497ee1c6b0ac
Debjit Naskar <45399832+captain-n3m0@users.noreply.github.com>
2024-08-24 06:05:37 +0530

    Fixed the bug in the lamp component.

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -17,7 +17,7 @@ export const LampContainer = ({
         className
       )}
     >
-      <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
+      <div className="relative flex w-full flex-1 sm:scale-y-100 scale-y-125 items-center justify-center isolate z-0 ">
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
